0dd761e8014b57dd41d96be98579e4c248308d1f
Emanuel Chareun <emachareun@gmail.com>
2022-02-16 17:42:13 -0300

    Todo listo, feito pero listo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "
+La Gran Muralla", address: "Hurlingham", phone_number: "011 4455 6677", category: "chinese")
+Restaurant.create(name: "La Parolaccia", address: "Puerto Madero", phone_number: "011 4793 1090", category: "italian")
+Restaurant.create(name: "Ishidan", address: "Palermo", phone_number: "011 5263 7777", category: "japanese")
+Restaurant.create(name: "Le Coq Dore", address: "Belgrano", phone_number: "011 4725 1606", category: "french")
+Restaurant.create(name: "Waffle House", address: "San Telmo", phone_number: "011 4831 9804", category: "belgian")
